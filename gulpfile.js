45b33ceaaff75b59c78dd9fbaa56ad4c9d580ab4
nlandkamer <nick@nicklandkamer.com>
2017-01-30 22:19:21 -0600

    added a lot to gulpfile and some other files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,56 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
     bs = require('browser-sync').create();
 
 gulp.task('sass', function(){
     gulp.src('./sass/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(bs.reload({stream: true}));
 });
 
-gulp.task('watch', ['sass'], function(){
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('scripts', function() {  
+    return gulp.src(['./js/**/*.js', '!./js/scripts.js'])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('build', ['sass', 'scripts'], function(){
+    return gulp.src(['./*.html', './css/**/*', './js/scripts.js', './lib/**/*', './htmlTemplates/**/*'],
+    {base: './'})
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('moveHtml', function(){
+    return gulp.src(['./*.html'], {base: './'})
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('moveCss', function(){
+    return gulp.src(['./css/**/*.*'], {base: './'})
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('moveJs', function(){
+    return gulp.src(['./js/scripts.js'], {base: './'})
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('moveTemplates', function(){
+    return gulp.src(['./htmlTemplates/**/*'], {base: './'})
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('moveLib', function(){
+    return gulp.src(['./lib/**/*.*'], {base: './'})
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
 });
 
 gulp.task('moveBower', function(){
@@ -17,6 +58,17 @@ gulp.task('moveBower', function(){
     .pipe(gulp.dest('./lib/'));
 });
 
-gulp.task('initialize', ['moveBower', 'sass']);
+gulp.task('watch', ['browser-sync'], function(){
+    gulp.watch('./*.html', ['moveHtml']);
+    gulp.watch('./htmlTemplates/**/*.html', ['moveTemplates']);
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./css/**/*.*', ['moveCss']);
+    gulp.watch('./js/scripts.js', ['moveJs']);
+    gulp.watch('./lib/**/*.*', ['moveLib']);
+    gulp.watch(['./js/**/*.js', '!./js/scripts.js'], ['scripts']);
+    gulp.watch('./dist/**/*.*').on('change', bs.reload);
+});
+
+gulp.task('initialize', ['moveBower', 'sass', 'scripts', 'build']);
 
 gulp.task('default', ['watch']);
