c4ff99a5d1800987921a24a7c9c6d225eb187a58
nlandkamer <nick@nicklandkamer.com>
2017-01-30 18:52:35 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    bs = require('browser-sync').create();
+
+gulp.task('sass', function(){
+    gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ['sass'], function(){
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('moveBower', function(){
+    gulp.src('./bower_components/**/*.*')
+    .pipe(gulp.dest('./lib/'));
+});
+
+gulp.task('initialize', ['moveBower']);
+
+gulp.task('default', ['watch']);
---
+++ js/main.js
@@ -0,0 +1,6 @@
+angular.module('MainApp', ['ui.bootstrap'])
+
+.component('mainHead', {
+    template: `<h1>Webpage Template with main-head Angular Component</h1>
+    <p>This is the good stuff!</p>`
+});
