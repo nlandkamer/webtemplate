3c216ee2c1261219aa93e323a80088296f26436d
nlandkamer <nick@nicklandkamer.com>
2017-02-03 17:46:19 -0600

    made things more component-like

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-angular.module('MainApp', ['ui.bootstrap'])
+angular.module('MainApp', ['ui.bootstrap', 'ngAnimate'])
 
 .component('mainHead', {
     templateUrl: 'htmlTemplates/mainHead.component.html'
+})
+
+.component('topNavbar', {
+    bindings: {
+        sitename: "@"
+    },
+    templateUrl: 'htmlTemplates/topNavbar.component.html',
+    controller: function(){
+        var $ctrl = this;
+        $ctrl.links = [
+            {
+                name: "Home",
+                url: "/"
+            },
+            {
+                name: "Contact",
+                url: "/Home/Contact"
+            },
+            {
+                name: "About",
+                url: "/Home/About"
+            }
+        ];
+        $ctrl.sitename = "Website Template";
+    }
+})
+
+.component('testStuff', {
+    templateUrl: 'htmlTemplates/testStuff.component.html'
 });
